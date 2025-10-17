7b710f18afec1b725b4369542cd87e4483715562
Samuel Reichert <samuelreichertt@gmail.com>
2025-10-17 15:32:04 +0200

    fix(datagrid-web): fix lint, test and build errors

--- packages/pluggableWidgets/datagrid-web/src/Datagrid.editorConfig.ts
+++ packages/pluggableWidgets/datagrid-web/src/Datagrid.editorConfig.ts
@@ -224,7 +224,8 @@ export const getPreview = (
                   allowEventPropagation: true,
                   exportValue: "",
                   exportType: "text",
-                  exportFormat: ""
+                  exportDateFormat: "",
+                  exportNumberFormat: ""
               }
           ];
     const columns = rowLayout({
